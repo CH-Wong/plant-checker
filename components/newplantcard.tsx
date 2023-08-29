202cc4beb195c860436a6497b807deeb67dabe18
CH-Wong <w.chunheung@gmail.com>
2023-08-29 12:40:46 +0200

    Squashed commit of the following:
    commit d7ab2f8aebfe5f105d92bc78955762e8f4fab4d6
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Tue Aug 29 12:40:39 2023 +0200
    
        added plant management options (adding/removing plants)
    
    commit 449e43c84aa0be850483d82156e0427974786325
    Merge: 9172f15 b10c53d
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Tue Aug 29 11:06:43 2023 +0200
    
        Merge branch 'main' into develop
    
    commit 9172f15fe34a924c87d67b13cc25ab18115becfc
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 23 20:16:55 2023 +0200
    
        added some CSS
    
    commit 02a508b8f7351ad6b0efa18a4381213efa97c3e1
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 23 19:24:28 2023 +0200
    
        split code into components
    
    commit bd0b50bc4c383154a1b79cc088ed5f1aa06b4cec
    Merge: fbd436a 1655714
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 23 19:19:23 2023 +0200
    
        Merge branch 'main' into develop
    
    commit fbd436ace8c240e84561b5e32d54bd8b6272f58e
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sat Apr 22 23:43:17 2023 +0200
    
        fixed typescript problems with new type class and added session persistence in login
    
    commit cd3e420f10b4ecc0918f1914923f95cb6c45a5f5
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 16 14:47:09 2023 +0200
    
        updated workflow
    
    commit 7280973f038c0c3e5d4c2526861e28429223af05
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Fri Apr 7 19:26:34 2023 +0200
    
        fixed some type issues and ran build for the first time
    
    commit c98d563a54a2f3289f9bbfd6c3af506d9a7983ac
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 2 20:26:45 2023 +0200
    
        how the heck does state lifting work with typescript
    
    commit 6138ba48b8731e3600217b96a71239ed5dfb5768
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Sun Apr 2 17:02:13 2023 +0200
    
        added some stuff
    
    commit fb4561d86326d973409a9f069d6bc85890e7fb15
    Author: CH-Wong <w.chunheung@gmail.com>
    Date:   Fri Mar 31 22:52:03 2023 +0200
    
        next.js + firebase

---
+++ components/newplantcard.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styles from './PlantCard.module.css';
+// import moment from 'moment';
+
+import { ref, set } from "firebase/database";
+import { database } from '@/common/config'
+
+
+type Plant = {
+  name: string,
+  plant_type: string,
+  last_watered: string,
+  water_interval: number,
+  last_pokon: string,
+  pokon_interval: number,
+  sun_required: string,
+};
+
+function addPlant(plant:Plant) {
+    set(ref(database, 'plants/' + plant.name), {
+        plant_type: plant.plant_type,
+        last_watered: plant.last_watered,
+        water_interval: plant.water_interval,
+        last_pokon: plant.last_pokon,
+        pokon_interval: plant.pokon_interval,
+        sun_required: plant.sun_required,
+    });
+    }
+
+
+function NewPlantCard() {
+  const [name, setName] = React.useState("");
+  const [plantType, setPlantType] = React.useState("");
+  const [sunRequired, setSunRequired] = React.useState("");
+  const [waterInterval, setWaterInterval] = React.useState(0);
+  const [pokonInterval, setPokonInterval] = React.useState(0);
+
+
+  function handleAdd(event:React.FormEvent<HTMLFormElement>) {
+    const newPlant:Plant = {
+      name: name,
+      plant_type: plantType,
+      last_watered:new Date().toISOString(),
+      water_interval: waterInterval*60*60*24,
+      last_pokon: new Date().toISOString(),
+      pokon_interval: pokonInterval*60*60*24,
+      sun_required: sunRequired,
+    }
+    addPlant(newPlant);
+  }
+
+  return (
+    <div className={styles.card}>
+      <h2>Add New Plant</h2>
+      <form onSubmit={handleAdd}>
+          <input 
+            name="name" 
+            type="text" 
+            placeholder="Plant Name" 
+            onChange={e=>setName(e.target.value)}
+          />
+
+          <input 
+            name="type" 
+            type="text" 
+            placeholder="Plant Type" 
+            onChange={e=>setPlantType(e.target.value)}
+          />
+          
+          <input 
+            name="water_interval" 
+            type="number" 
+            placeholder="Water Interval (days)"
+            onChange={e=>setWaterInterval(Number(e.target.value))}
+          />
+
+          <input 
+            name="pokon_interval" 
+            type="number" 
+            placeholder="Pokon Interval (days)"
+            onChange={e=>setPokonInterval(Number(e.target.value))}
+          />
+
+          &nbsp;
+          <label>Sun Required</label>
+          <select name="sun_required" onChange={e=>setSunRequired(e.target.value)}>
+            <option value="Full Sun">Full Sun</option>
+            <option value="Half Shade">Half Shade</option>
+            <option value="Full Shade"> Full Shade</option>
+          </select>  
+
+          <button type="submit">Add Plant</button>
+      </form>
+      <div className={styles.bar} />
+      {/* <button type="submit" onClick={handleAdd}>
+        Add Plant
+      </button> */}
+    </div>
+  );
+};
+
+export default NewPlantCard;
